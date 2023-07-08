983fcd941e36db846a39feee2c050d42b878fc37
DSW-JulianGuevara-1392 <130520435+DSW-JulianGuevara-1392@users.noreply.github.com>
2023-07-07 22:47:27 -0500

    Ejercicio de la fase 4
    Se adiciona cuestionario.

---
+++ fase3/fase4/js/script1.js
@@ -0,0 +1,27 @@
+function procesarrespuestas()
+{
+    let total = 5
+    let puntos = 0;
+
+    let myform = document.forms["formulario"];
+    let respuestascorrectas = ["b","a","c","c","a"];
+
+    for (let i = 1; i <= total; i++)
+    {
+        if (myform["r" + i].value == null || myform["r" + i].value == '')
+        {
+            alert('Favor responder todas las preguntas');
+            return false;
+        }
+        else
+        {
+            if (myform["r" + i].value === respuestascorrectas[i-1])
+            {
+                puntos++;
+            }
+        }
+    }
+    let resultado = document.getElementById('resultado')
+    resultado.innerHTML = 'Obtuviste ' + puntos + ' puntos de ' + total + ' posibles.';
+    return false;
+}
